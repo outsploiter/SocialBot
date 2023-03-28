83e2e99c169ec4f23c99bd9216071319913dce7b
outsploiter <suweeharshan@gmail.com>
2023-03-28 19:46:52 +0530

    movie caption len fix

--- utils/uploader.py
+++ utils/uploader.py
@@ -18,7 +18,7 @@ def upload_to_insta(file_path, profile):
     if os.path.exists(caption_path):
         with open(caption_path, encoding='utf-8') as file:
             caption = file.read()
-    if caption is not None and len(caption)<2200:
+    if caption is not None and len(caption)>2000:
         print('')
         caption = '''Look at this amazing video\n#movies #movie #flim #instagood'''
 
